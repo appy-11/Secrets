739c38180321ce141584353e2318a110d4ed1ac9
Apoorva Sharma <kannusharma524@gmail.com>
2020-07-17 14:52:38 +0530

    Update

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose =  require("mongoose");
-const md5 = require("md5");
+const session = require("express-session");
+const passport =  require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const app = express();
 
 
@@ -17,18 +20,31 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+  secret: 'Our little secret.',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema( {
     email:String,
     password:String
-});0
+});
 
-
-
-
+userSchema.plugin(passportLocalMongoose);
 const User = new mongoose.model("User", userSchema);
 
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res){
    res.render("home"); 
@@ -44,35 +60,11 @@ app.get("/register",function(req,res){
 
 
 app.post("/register", function(req,res){
-   const newUser =  new User({
-      email: req.body.username,
-      password: md5(req.body.password)
-   });
-   newUser.save(function(err){
-       if(err){
-           console.log(err);
-       }else{
-           res.render("secrets");
-       }
-   }); 
+  
 });
 
 
 app.post("/login",function(req,res){
-const username = req.body.username;
-const password = md5(req.body.password);
-    
-User.findOne({email:username},function(err,foundUser){
-   if(err){
-       console.log(err);
-   }else{
-       if(foundUser){
-           if(foundUser.password === password){
-               res.render("secrets");
-           }
-       }
-   } 
-  });
    
 });
 
